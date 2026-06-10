c0115174adb054d6183223a8dd59a806306e04dd
Shreyas Damase <shreyasdamase@example.com>
2026-06-10 13:10:34 +0530

    Add networking and UI dependencies and implement CoroutineLearningScreen
    - Add Retrofit, OkHttp, Gson, Coil, and Coroutines dependencies in `build.gradle.kts` and `libs.versions.toml`.
    - Add `INTERNET` permission and enable cleartext traffic in `AndroidManifest.xml`.
    - Update `MainActivity` to initialize `CoroutineViewModel` and display `CoroutineLearningScreen`.
    - Register `LegacyActivity` in the manifest and update the app theme to inherit from `Theme.AppCompat`.
    - Update IDE configuration files.

--- app/src/main/java/com/example/studyapp/MainActivity.kt
+++ app/src/main/java/com/example/studyapp/MainActivity.kt
@@ -1,47 +1,44 @@
 package com.example.studyapp
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.studyapp.ui.theme.StudyappTheme
+import androidx.compose.material3.Surface
+import com.example.studyapp.ui.coroutineScreen.CoroutineLearningScreen
+import com.example.studyapp.ui.coroutineScreen.CoroutineViewModel
+ 
 
 class MainActivity : ComponentActivity() {
+    //    lateinit var coroutineViewModel: CoroutineViewModel 2nd way
+    val viewModel: CoroutineViewModel by viewModels()
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        coroutineViewModel = ViewModelProvider(this)[CoroutineViewModel::class.java] 2nd way
+
         enableEdgeToEdge()
         setContent {
             StudyappTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+                    Surface(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                    ) {
+                        CoroutineLearningScreen(viewModel = viewModel, modifier = Modifier)
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    StudyappTheme {
-        Greeting("Android")
     }
 }
